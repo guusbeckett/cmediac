3e981d465b732681ab0c5f092f7f36af36d918a1
Sven <sven@memcmp.org>
2013-05-09 01:06:35 +0200

    Added basic plugin system.

--- cmediac.py
+++ cmediac.py
@@ -1,4 +1,7 @@
 import urwid
+import os
+import glob
+import imp
 
 palette = [
     (None,  'light gray', 'black'),
@@ -19,19 +22,30 @@ class MenuButton(urwid.Button):
     def __init__(self, text):
         super(MenuButton, self).__init__(text)
         self._w = urwid.AttrMap(self._w, None, 'selected')
+        
+class Plugin(MenuButton):
+    def __init__(self, filename):
+        name = os.path.basename(filename)[:-3]
+        fp, pathname, description = imp.find_module(name, [os.path.dirname(filename)])
+        plugin = imp.load_module(name, fp, pathname, description)
+        super(Plugin, self).__init__(plugin.get_name())
 
-class Menu(urwid.ListBox):
+class Menu(urwid.WidgetWrap):
     def __init__(self, caption, choices):
         header = urwid.AttrMap(urwid.Text(caption, align='center'), 'heading')
-        super(Menu, self).__init__(urwid.SimpleFocusListWalker([header, urwid.Divider()] + choices))
+        self._w = urwid.AttrMap(urwid.ListBox(urwid.SimpleFocusListWalker([header, urwid.Divider()] + choices)), 'options', focus_map)
 
 def main():
     columns = urwid.Columns([], dividechars=1)
     
-    columns.contents.append((urwid.AttrMap(Menu('Plugins', [MenuButton('Uitzending Gemist'), MenuButton('YouTube')]), 'options', focus_map),
-                             columns.options('given', 24)))
-    columns.contents.append((urwid.AttrMap(Menu('YouTube', [MenuButton('Problems with Zero - Numberphile'), MenuButton('Infite Primes - Numberphile')]), 'options', focus_map),
-                             columns.options('weight', 24)))
+    plugins = []
+    
+    for filename in glob.glob('plugins/*.py'):
+        plugins.append(Plugin(filename))
+    
+    columns.contents.append((Menu('cmediac', [MenuButton('Exit')] + plugins), columns.options('given', 24)))
+#   columns.contents.append((urwid.AttrMap(Menu('YouTube', [MenuButton('Problems with Zero - Numberphile'), MenuButton('Infite Primes - Numberphile')]), 'options', focus_map),
+#                            columns.options('weight', 24)))
     
     urwid.MainLoop(columns, palette).run()
     
