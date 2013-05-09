80f3289a5d0f367e42f041078f7d15bd7a60aa47
Sven <sven@memcmp.org>
2013-05-09 20:23:08 +0200

    Now rips the actual video url.

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.parse
 import re
 
 def get_name():
@@ -10,10 +11,7 @@ def get_links():
     with open("youtube-channels.txt") as f:
         for channel in f:
             feed = urllib.request.urlopen('http://gdata.youtube.com/feeds/api/users/' + channel + '/uploads') 
-            
-            data = feed.read()
-            data = data.decode()
-            
+            data = feed.read().decode()
             items = re.findall("<title type='text'>([^<>]+)</title><content type='text'>[^<>]+</content><link rel='alternate' type='text/html' href='([^']+)'/>", data)
             
             for item in items:
@@ -22,4 +20,13 @@ def get_links():
     return links 
 
 def get_media(url):
-    return url
+    tokens = urllib.parse.parse_qs(urllib.parse.urlparse(url).query)
+    
+    file = urllib.request.urlopen('http://www.youtube.com/get_video_info?&video_id=' + tokens['v'][0] + '&ps=default&eurl=&gl=US&hl=en')
+    video_info = urllib.parse.parse_qs(file.read().decode())
+    
+    url_data_strs = video_info['url_encoded_fmt_stream_map'][0].split(',')
+    url_data = [urllib.parse.parse_qs(uds) for uds in url_data_strs]
+    video = url_data[0] # always pick the first; this might need a little more research
+
+    return video['url'][0] + '&signature=' + video['sig'][0]
