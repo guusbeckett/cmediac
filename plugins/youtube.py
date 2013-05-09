3879a1c2f150bb0f7c41420c651b8073ec8a0e35
Sven <sven@memcmp.org>
2013-05-09 19:23:30 +0200

    Neater code.

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -10,11 +10,14 @@ def get_links():
     with open("youtube-channels.txt") as f:
         for channel in f:
             feed = urllib.request.urlopen('http://gdata.youtube.com/feeds/api/users/' + channel + '/uploads') 
+            
             data = feed.read()
             data = data.decode()
-            titles = re.findall("<title type='text'>([^<>]+)</title><content type='text'>[^<>]+</content><link rel='alternate' type='text/html' href='([^']+)'/>", data)
-            for title in titles:
-                links.append((title[0], title[1]))
+            
+            items = re.findall("<title type='text'>([^<>]+)</title><content type='text'>[^<>]+</content><link rel='alternate' type='text/html' href='([^']+)'/>", data)
+            
+            for item in items:
+                links.append(item)
     
     return links 
 
