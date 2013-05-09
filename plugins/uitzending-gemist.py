3c535d8e12cf9aadc0a1dad7c2559edfce052abd
Sven <sven@memcmp.org>
2013-05-09 10:10:33 +0200

    Implemented working channel listing.

--- plugins/uitzending-gemist.py
+++
@@ -1,8 +0,0 @@
-def get_name():
-    return 'Uitzending Gemist'
-
-def get_links():
-    return [('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl')]
-
-def get_media(url):
-    return "hello"
--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,8 +1,21 @@
+import urllib
+import re
+
 def get_name():
     return 'YouTube'
 
 def get_links():
-    return [('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl'), ('The monster', 'http://www.nu.nl')]
+    links = []
+    
+    with open("youtube-channels.txt") as f:
+        for channel in f:
+            feed = urllib.urlopen('http://gdata.youtube.com/feeds/api/users/' + channel + '/uploads') 
+            data = feed.read()
+            titles = re.findall("<title type='text'>([^<>]+)</title><content type='text'>[^<>]+</content><link rel='alternate' type='text/html' href='([^']+)'/>", data)
+            for title in titles:
+                links.append((title[0], title[1]))
+    
+    return links 
 
 def get_media(url):
-    return "hello"
+    return url
