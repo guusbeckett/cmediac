bc0535b37e0a0a2ae3b4388cbcb5e1a9d17d7d05
Sven <sven@memcmp.org>
2013-05-12 01:46:45 +0200

    Fixed error.

--- plugins/tedtalks.py
+++ plugins/tedtalks.py
@@ -14,4 +14,4 @@ class Plugin:
         
     def get_items(self):
         feed = feedparser.parse('http://feeds.feedburner.com/tedtalks_video')
-        return [Media(talk["title"], talk["feedburner:origEnclosureLink"]) for talk in feed["items"]]
+        return [Media(talk["title"], talk["links"][1]["href"]) for talk in feed["items"]]
