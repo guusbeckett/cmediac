12748a3b724a30e5a4ddd76b471c5db4768d34a6
Sven <sven@memcmp.org>
2013-05-12 01:23:19 +0200

    Added TED talks plugin.

---
+++ plugins/tedtalks.py
@@ -0,0 +1,17 @@
+import feedparser
+
+class Media:
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+        
+    def get_url(self):
+        return self.url
+
+class Plugin:
+    def __init__(self, config):
+        self.title = 'TED Talks'
+        
+    def get_items(self):
+        feed = feedparser.parse('http://feeds.feedburner.com/tedtalks_video')
+        return [Media(talk["title"], talk["link"]) for talk in feed["items"]]
